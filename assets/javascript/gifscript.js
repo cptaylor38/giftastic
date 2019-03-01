cd874f2b5d245899115a682a1a9bec6096f765f9
Corbin Taylor <cptaylor38@yahoo.com>
2019-03-01 02:55:59 -0500

    still want to add conditional to require limit being set. still need pause and play functionality and graphical improvements

--- assets/javascript/gifscript.js
+++ assets/javascript/gifscript.js
@@ -1,34 +1,80 @@
 var categoryCount = 1;
+var categoryArray = [];
+
+categoryArray = JSON.parse(localStorage.getItem('categoryArray'));
+// Push the new data (whether it be an object or anything else) onto the array
+localStorage.setItem('categoryArray', JSON.stringify(categoryArray));
+
+
+
+
+
+
 
 
 queryURL = "https://api.giphy.com/v1/gifs/search?";
 
+window.onload = function () {
+    var $savedCategories = JSON.parse(localStorage.getItem('categoryArray'));
+    console.log($savedCategories);
+    if ($savedCategories.length === 0) {
+
+    }
+    else {
+        $savedCategories.forEach(element => {
+            $gifSelect = $('#gifCategories');
+            $categoryButton = $('<button>');
+            $categoryButton.text(element);
+            $categoryButton.attr({
+                'id': 'item-' + categoryCount,
+                'data-category': element
+            });
+            $categoryButton.addClass('gifButton');
+            $gifSelect.append($categoryButton);
+        });
+    }
+}
 
 
 $("#categoryAddButton").on("click", function (event) {
     event.preventDefault();
 
-    var $categoryAdd = $('#categoryAdd').val();
-
-    $gifSelect = $('#gifCategories');
-    $categoryButton = $('<button>');
-    $categoryButton.text($categoryAdd);
-    $categoryButton.attr({
-        'id': 'item-' + categoryCount,
-        'data-category': $categoryAdd
-    });
-    $categoryButton.addClass('gifButton');
-
-
-    $gifSelect.append($categoryButton);
-
+    var $categoryAdd = $('#categoryAdd').val().toLowerCase();
+    if ($categoryAdd === '') {
+        alert("Cannot submit an empty category.");
+    }
+    else {
+        addCategoryButton($categoryAdd);
+    }
 });
+
+addCategoryButton = function ($categoryAdd) {
+
+    if (categoryArray.includes($categoryAdd)) {
+        console.log('used this');
+    }
+    else {
+        categoryArray.push($categoryAdd);
+        localStorage.setItem('categoryArray', JSON.stringify(categoryArray));
+        $gifSelect = $('#gifCategories');
+        $categoryButton = $('<button>');
+        $categoryButton.text($categoryAdd);
+        $categoryButton.attr({
+            'id': 'item-' + categoryCount,
+            'data-category': $categoryAdd
+        });
+        $categoryButton.addClass('gifButton');
+        $gifSelect.append($categoryButton);
+    }
+
+}
 
 $(document.body).on('click', '.gifButton', function (event) {
     event.preventDefault();
 
     var $gifLoad = $('#gifLoad');
     $gifLoad.empty();
+    var $limit = $('#limitSelect').val();
 
     var $gifCategory = $(this).attr('data-category');
 
@@ -38,7 +84,7 @@ $(document.body).on('click', '.gifButton', function (event) {
         data: {
             q: '' + $gifCategory,
             apikey: 'fcEH4tpKk10gcbgphb7MecpbedH4Hq4U',
-            limit: '6'
+            limit: $limit
         }
     }).then(function (response) {
 
